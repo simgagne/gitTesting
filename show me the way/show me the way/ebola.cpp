3c2914b87980c4d56fab3546054b42e1f101d4f9
simgagne <35602056+simgagne@users.noreply.github.com>
2018-01-19 10:10:39 -0500

    wazza

---
+++ show me the way/show me the way/ebola.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include<cctype>
+using namespace std;
+
+int main() {
+	int i = 0;
+	while (1 == 1) {
+		int i = 0;
+		cout << 1;
+		while (i == 0) {
+			cout << 0;
+			i = 1;
+		}
+
+	}
+
+
+
+
+
+	return 0;
+}
