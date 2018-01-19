4f50bb54eec13019654cc009f38cb479c0475dd9
Monkey2.0 <hassaanbahsoun@hotmail.com>
2018-01-19 10:38:20 -0500

    NEW UPDATE BRUDDA
    nigger faggot

--- show me the way/show me the way/ebola.cpp
+++ show me the way/show me the way/ebola.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 int main() {
-	int i = 0;
-	while (1 == 1) {
+	for (int k = 0; k < 30; k++) {
 		int i = 0;
-		cout << 1;
-		while (i == 0) {
-			cout << 0;
-			i = 1;
-		}
+		while (1 == 1) {
+			int i = 0;
+			cout << 1;
+			while (i == 0) {
+				cout << 0;
+				i = 1;
+			}
 
+		}
 	}
+
+	cout << " SHOW ME DE WAE BRUDDA ";
 
 
 
